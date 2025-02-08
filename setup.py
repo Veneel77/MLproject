c6f1c7ea3430b1e0c7b725db097efacc001f0009
Veneel77 <veneeldas77@gmail.com>
2025-02-09 01:04:26 +0530

    logging and exception

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+##from typing import List
+
+   #HYPEN_E_DOT='-e .'
+   #def get_requirements(file_path:str)->List[str]:
+
+    
+    #requirements=[]
+    #with open('C:\\Users\\Veneel\\OneDrive\\Documents\\GitHub\\MLproject\\requirements.txt') as file_obj:
+       # requirements=file_obj.readlines(),
+      #  requirements=[req.replace("\n"," ") for req in requirements]
+            ##return [req.strip() for req in requirements if req.strip() and not req.startswith('#')]
+
+
+     #   if HYPEN_E_DOT in requirements:
+    #        requirements.remove(HYPEN_E_DOT)
+            ##return [req.strip() for req in requirements if req.strip() and not req.startswith('#')]
+
+        
+
+   # return requirements)
+#
+packages=find_packages( ),
+install_requires=['pandas','numpy','seaborn'],
+#print(install_requires)
+
+
+
+
+
+
